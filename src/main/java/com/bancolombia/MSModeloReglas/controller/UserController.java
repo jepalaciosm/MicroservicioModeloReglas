fc836bc1f8d3efb1e32733fde9ba76ad1c945e20
John Edwar Palacios <johnpalaciso23@gmail.com>
2025-05-15 22:59:26 -0500

    fix: Ajuste en metodos nuevos

--- src/main/java/com/bancolombia/MSModeloReglas/controller/UserController.java
+++ src/main/java/com/bancolombia/MSModeloReglas/controller/UserController.java
@@ -2,18 +2,18 @@ package com.bancolombia.MSModeloReglas.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bancolombia.MSModeloReglas.entities.ComercialEntity;
 import com.bancolombia.MSModeloReglas.service.IComercialService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
